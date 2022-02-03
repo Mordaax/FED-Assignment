019ff2795c3f754d66931ee1a97844920f83623b
Jie En Javien Tan <s10221841@connect.np.edu.sg>
2022-02-04 02:52:56 +0800

    Dessert Stall (not completed)

---
+++ Dessert Stall/js/dessertScript.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+// for collapsible text
+function ShowMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
