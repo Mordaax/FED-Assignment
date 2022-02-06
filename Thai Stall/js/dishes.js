d6616597493b585eaa51f0143786d05798aefa3f
eugen-io <94427178+eugen-io@users.noreply.github.com>
2022-02-06 11:17:39 +0800

    [Thai] Added images to dishes

--- Thai Stall/js/dishes.js
+++ Thai Stall/js/dishes.js
@@ -3,51 +3,60 @@ const dishdata = {
         {
            "Name": "Fried Chicken Wings (8 pc)",
            "Description": "Freshly fried wings",
-           "Price": 4.5 
+           "Price": 4.5,
+           "Image": "images/chickenwings.jpeg"
         },
         {
             "Name": "Egg Roll (4 pc)",
             "Description": "These rolls contain ground chicken, pork, carrots, and glass noodles",
-            "Price": 5.9 
+            "Price": 5.9,
+            "Image": "images/eggrolls.jpg"
         },
         {
             "Name": "Fried Beef Meatballs",
             "Description": "Try our signature balls today!",
-            "Price": 4.5 
+            "Price": 4.5,
+            "Image": "images/meatballs.jpg"
         },
     ],
     "MAINS": [
         {
            "Name": "Pad Thai",
            "Description": "Try out our famous pad thai noodles!",
-           "Price": 7.9 
+           "Price": 7.9,
+           "Image": "images/padthai.jpg"
         },
         {
             "Name": "Tom Yum Goong",
             "Description": "Who could resist having our signature Tom Yum Goong?",
-            "Price": 6.5 
+            "Price": 6.5,
+            "Image": "images/tomyumgoong.jpg"
         },
         {
             "Name": "Green Curry",
             "Description": "Our green curry stems from a generations-old recipe!",
-            "Price": 12.9 
+            "Price": 12.9,
+            "Image": "images/greencurry.jpeg"
         },
     ],
     "DRINKS": [
         {
            "Name": "Thai Iced Milk Tea",
            "Description": "Our iced tea has been the talk of the town!",
-           "Price": 4.9 
+           "Price": 4.9,
+           "Image": "images/milktea.jpg"
         },
         {
             "Name": "Thai Iced Green Tea",
             "Description": "This version of milk tea contains green tea, providing an aromatic experience!",
-            "Price": 4.9 
+            "Price": 4.9,
+            "Image": "images/greentea.jpg"
         },
         {
             "Name": "Water",
             "Description": "Was the green curry too spicy? Over here, our water is unlimited for $0.50!",
-            "Price": 0.5 
+            "Price": 0.5,
+            "Image": "images/water.jpg"
         },
     ]
 };
--- Thai Stall/js/modalcontroller.js
+++ Thai Stall/js/modalcontroller.js
@@ -1,6 +1,10 @@
 const tmp = "\
-<div class=\"card col-sm-12\">\
-    <div class=\"card-header bg-transparent\">{NAME}    (${PRICE})</div>\
+<div class=\"card col-sm-12 p-2\">\
+    <img class=\"img-fluid card-img-top\" src=\"{IMG}\">\
+    <div class=\"card-header bg-transparent\">\
+        <h5 class=\"card-title\">{NAME}</h5>\
+        <h6 class=\"card-subtitle\">${PRICE}</h6>\
+    </div >\
     <div class=\"card-body\">\
         <p class=\"card-text\">{DESCRIPTION}</p>\
     </div>\
@@ -16,7 +20,8 @@ function create_modals(dishcat, id) {
         var content = tmp
             .replace("{NAME}", dish.Name)
             .replace("{PRICE}", dish.Price.toFixed(2))
-            .replace("{DESCRIPTION}", dish.Description);
+            .replace("{DESCRIPTION}", dish.Description)
+            .replace("{IMG}", dish.Image);
 
         modal.innerHTML += content;
     }
